4ffc5e0d8342c48ed1bb10c6d43ef065df982ee2
anjana-dodampe <ahmdodampe@hotmail.com>
2020-07-05 00:13:49 +0530

    ifelse

---
+++ ifElse.py
@@ -0,0 +1,13 @@
+num = input()
+
+x = int(num)
+
+if x / 2 != 0:
+    print('Weird')
+else:
+    if x >= 2 and x <= 5:
+        print('Not Weird')
+    elif x >= 6 and x <= 20:
+        print('Weird!')
+    else:
+        print('Not Weird!')
